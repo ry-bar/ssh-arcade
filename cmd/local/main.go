a7e35e97c4595deb3779fd6cfb7cc84632af820f
Ryan Bartel <bartrya2@isu.edu>
2026-08-22 16:24:37 -0600

    screen switches to snake game

--- cmd/local/main.go
+++ cmd/local/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 
 	tea "charm.land/bubbletea/v2"
+
+	"github.com/ry-bar/ascii-run/internal/games/snake"
 )
 
 
@@ -14,10 +16,22 @@ import (
 // Update(tea.Msg) (tea.Model, tea.Cmd)
 // View() string
 
+func returnMainMenu(m model) tea.Msg {
+
+	m.activeGame = nil
+	m.currentScreen = menuScreen
+
+	return m
+}
+
+
+
 type model struct {
 	currentScreen screen
 	games []string
 	cursorLocation int
+
+	activeGame tea.Model
 }
 
 type screen int
@@ -33,6 +47,8 @@ func initialModel() model{
 		currentScreen: menuScreen,
 		games: []string{"Tic-Tac-Toe", "Snake"},
 		cursorLocation: 0,
+
+		activeGame: nil,
 	}
 }
 
@@ -41,40 +57,65 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type){
-		case tea.KeyPressMsg:
-			switch(msg.String()) {
-				case "w", "up", "k":
-					if m.cursorLocation > 0{
-						m.cursorLocation--
-					}
-				case "s", "down", "j":
-					if m.cursorLocation < len(m.games)-1 {
-						m.cursorLocation++
-					}
-				case "enter", "space":
-					
-				case "q", "ctrl+c":
-					return m, tea.Quit
-			
-			}
-	}
-	return m, nil
 
+	if m.currentScreen == snakeScreen {
+		updatedGame, cmd := m.activeGame.Update(msg)
+		m.activeGame = updatedGame
+
+		return m, cmd	
+	}
+
+	if m.currentScreen == menuScreen {
+		switch msg := msg.(type){
+			case tea.KeyPressMsg:
+				switch(msg.String()) {
+					case "w", "up", "k":
+						if m.cursorLocation > 0{
+							m.cursorLocation--
+						}
+					case "s", "down", "j":
+						if m.cursorLocation < len(m.games)-1 {
+							m.cursorLocation++
+						}
+					case "enter", "space":
+						if m.cursorLocation == 1 {
+							m.currentScreen = snakeScreen
+							m.activeGame = snake.New()
+
+							return m, m.activeGame.Init()
+						}
+					case "q", "ctrl+c":
+						return m, tea.Quit
+				
+				}
+		}
+	}
+
+	return m, nil
 }
 
 func (m model) View() tea.View {
-	s := "\nSelect a game:\n\n"
+	s := ""
 
-	for i, choice := range m.games {
-		cursor := " "
-		if m.cursorLocation == i {
-			cursor = ">"
-		}
-		s += fmt.Sprintf("%s %s\n", cursor, choice)
+	if m.currentScreen == snakeScreen {
+		return m.activeGame.View()
 	}
-		
-	s += "\n\nPress ctrl+c or q to quit"
+
+
+	if m.currentScreen == menuScreen {
+		s = "\nSelect a game:\n\n"
+
+		for i, choice := range m.games {
+			cursor := " "
+			if m.cursorLocation == i {
+				cursor = ">"
+			}
+			s += fmt.Sprintf("%s %s\n", cursor, choice)
+		}
+
+		s += "\n\nPress ctrl+c or q to quit"
+	}
+
 
 	return tea.NewView(s)
 }
--- internal/games/snake/snake.go
+++ internal/games/snake/snake.go
@@ -1,3 +1,115 @@
 // This is where the snake game state logic will be written.
 
 package snake
+
+import (
+	"strings"
+//  "fmt"
+
+  tea "charm.land/bubbletea/v2"
+)
+
+
+type point struct {
+	x int
+	y int
+}
+
+type direction int
+
+const (
+	up direction = iota
+	down
+	left
+	right
+)
+
+type model struct {
+	width int
+	height int
+
+	snake []point
+	food point
+
+	direction direction
+
+	gameOver bool
+	score int
+}
+
+
+type stopGame string
+
+
+func stop() tea.Msg {
+	return stopGame("stop")
+}
+
+
+func New() model {
+	return model{
+		width: 40,
+		height: 20,
+
+		snake: []point{
+			{x:12,y:12},
+			{x:12,y:13},
+		},
+		food: point{x:6,y:6},
+
+		gameOver: false,
+		score: 0,
+	}
+}
+
+
+
+func (m model) Init() tea.Cmd {
+	// Maybe this is where I would gather the user data that's needed for the game?
+	return nil
+}
+
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+
+	switch msg := msg.(type) {
+		case tea.KeyPressMsg:
+			switch msg.String() {
+				case "q", "crtl+c":
+					return m, tea.Quit
+			}
+
+	}
+ 
+	return m, nil
+}
+
+
+
+//TODO: This function is hella broken
+func (m model) View() tea.View{
+
+	s := strings.Repeat("-", m.width) + "\n"
+
+
+	internalWidth := m.width - 2
+	if internalWidth < 0 {
+		internalWidth = 0
+	}
+
+	internalHeight := m.height - 2
+	if internalHeight < 0 {
+		internalHeight = 0
+	}
+
+
+	for i := 0; i < internalHeight; i++ {
+		s += "|" + strings.Repeat(" ", internalWidth) + "|\n"
+	}
+
+	s += strings.Repeat("-", m.width)
+	
+
+	return tea.NewView(s)
+}
+
